02a6139e4680d638edab0e85c1f0194e23718212
Insasse <schmidt.dirk33@gmail.com>
2017-09-14 15:45:13 +0200

    fix undefined error

--- js/thsixty.load.js
+++ js/thsixty.load.js
@@ -5,6 +5,7 @@
 (function ($, window, document) {
   Drupal.behaviors.thsixty = {
     attach: function (context) {
+      let env = drupalSettings.hm_newsletter ? drupalSettings.hm_newsletter.env : false;
       var config = {
         env: drupalSettings.hm_newsletter.env,
         version: 'v1'
@@ -14,11 +15,13 @@
       var th = document.createElement('script');
       th.type = 'text/javascript';
       th.async = true;
-      if (drupalSettings.hm_newsletter.env === 'staging') {
-        th.src = '//d2528hoa8g0iaj.cloudfront.net/staging/thsixty.min.js';
-      }
-      else {
-        th.src = '//d2528hoa8g0iaj.cloudfront.net/thsixty.min.js';
+      if (env) {
+        if (drupalSettings.hm_newsletter.env === 'staging') {
+          th.src = '//d2528hoa8g0iaj.cloudfront.net/staging/thsixty.min.js';
+        }
+        else {
+          th.src = '//d2528hoa8g0iaj.cloudfront.net/thsixty.min.js';
+        }
       }
       var s = document.getElementsByTagName('script')[0];
       s.parentNode.insertBefore(th, s);
